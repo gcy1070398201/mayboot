08031e73bfe0016d807f3f1114d6671628cb52dd
gu1070398201@sina.com <gu159357>
2018-04-19 16:29:53 +0800

    创建Spring Boot 项目

---
+++ src/test/java/com/gcy/MaybootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gcy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaybootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
